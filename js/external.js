7bc41b85a6fb127cb66458bc2950e678ccd4539f
Felecia Davis <felecia.m.davis1@gmail.com>
2021-02-02 13:53:43 -0500

    feat: using alert and prompt on inline and external javascript files. Also using alert and prompt while calucating the total price of 3 movies for a number of days supplied by the prompt.

--- js/external.js
+++ js/external.js
@@ -1,1 +1,15 @@
 console.log("Hello from External Javascript.")
+alert("Welcome to my website");
+var userInput = prompt('What is your favorite color?');
+alert("Great " + userInput + " is my favorite color also.");
+
+var price = 3.00
+var lm = prompt('How many days are you renting this movie?');
+alert("Little mermaid for " + lm + " days.");
+var bb = prompt('How many days are you renting this movie?');
+alert("Brother Bear for " + bb + " days.");
+var Herc = prompt('How many days are you renting this movie?');
+alert("Hercules for " + Herc +" days.");
+var totpay = (lm + bb+ Herc) * price;
+alert("You will have to pay $" + totpay + " for your movies.");
+
