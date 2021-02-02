d8072eb93b3c544daace9a82bb801a7c3aaae4d7
Felecia Davis <felecia.m.davis1@gmail.com>
2021-02-02 16:05:47 -0500

    feat: correct calculation on movie exercise and add exercise for class enrollment.

--- js/external.js
+++ js/external.js
@@ -1,33 +1,39 @@
-console.log("Hello from External Javascript.")
-alert("Welcome to my website");
-var userInput = prompt('What is your favorite color?');
-alert("Great " + userInput + " is my favorite color also.");
+// console.log("Hello from External Javascript.")
+// alert("Welcome to my website");
+// var userInput = prompt('What is your favorite color?');
+// alert("Great " + userInput + " is my favorite color also.");
 
 var price = 3.00
 var lm = prompt('How many days are you renting this movie?');
+console.log(typeof lm);
 alert("Little mermaid for " + lm + " days.");
 var bb = prompt('How many days are you renting this movie?');
 alert("Brother Bear for " + bb + " days.");
 var Herc = prompt('How many days are you renting this movie?');
 alert("Hercules for " + Herc +" days.");
-var totpay = (lm + bb+ Herc) * price;
+var totpay = (Number(lm)+ Number(bb )+ Number(Herc)) * price;
 alert("You will have to pay $" + totpay + " for your movies.");
 
-var googlepay = 400;
-var amazonpay = 380;
-var facebookpay = 350;
+// var googlepay = 400;
+// var amazonpay = 380;
+// var facebookpay = 350;
+//
+// var googlehours = prompt('How many hours did you work for Google?');
+// alert(googlehours + " hours worked for Google.");
+// var googletotal = googlepay * googlehours;
+// alert("Total amount earned from Google is " + googletotal);
+// var amazonhours = prompt('How many hours did you work for Amazon');
+// alert(amazonhours + " hours worked for Amazon.");
+// var amazontotal = amazonpay * amazonhours;
+// alert("Total amount earned from Google is " + amazontotal);
+// var facebookhours = prompt('How many hours did you work for Facebook');
+// alert(facebookhours + " hours worked for Facebook.");
+// var facebooktotal = facebookpay * facebookhours;
+// alert("Total amount earned from Google is " + facebooktotal);
+// var totalpay = googletotal + amazontotal + facebooktotal;
+// alert("Total for all is $" +  totalpay);
+//
+// var classnotfull = confirm('Is there room in the class for another student?');
+// var noscheduleconflict = confirm('Does your schedule work with class schedule?');
+// alert("You can enroll in class is a " + (classnotfull && noscheduleconflict) + " statemnent.");
 
-var googlehours = prompt('How many hours did you work for Google?');
-alert(googlehours + " hours worked for Google.");
-var googletotal = googlepay * googlehours;
-alert("Total amount earned from Google is " + googletotal);
-var amazonhours = prompt('How many hours did you work for Amazon');
-alert(amazonhours + " hours worked for Amazon.");
-var amazontotal = amazonpay * amazonhours;
-alert("Total amount earned from Google is " + amazontotal);
-var facebookhours = prompt('How many hours did you work for Facebook');
-alert(facebookhours + " hours worked for Facebook.");
-var facebooktotal = facebookpay * facebookhours;
-alert("Total amount earned from Google is " + facebooktotal);
-var totalpay = googletotal + amazontotal + facebooktotal;
-alert("Total for all is $" +  totalpay);
