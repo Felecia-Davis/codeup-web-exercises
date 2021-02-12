5b222ab74af289b674e078c5aa7e725c8d77d601
Felecia Davis <felecia.m.davis1@gmail.com>
2021-02-12 05:46:50 -0500

    feat: completed while and do-while exercise.

--- js/while.js
+++ js/while.js
@@ -1,10 +1,11 @@
 
     "use strict"
-    // var i = 1;
-    // while(i < 65536){
-    //     i = i * 2;
-    //     console.log(i);
-    // }
+
+    var i = 1;
+    while(i < 65536){
+        i = i * 2;
+        console.log(i);
+    }
 
 
 // This is how you get a random number between 50 and 100
@@ -14,20 +15,23 @@
     var totalSold = 0;
     var conesRemaining = 0;
    do {
+       // This expression will generate a random number between 1 and 5
        var numEachCustomer = Math.floor(Math.random() * 5) + 1;
-       console.log(numEachCustomer);
+       console.log(numEachCustomer + " cone(s) requested by customer.");
        conesRemaining = allCones - totalSold;
-       console.log(conesRemaining + " cone remaining.");
-       // totalSold =  totalSold + numEachCustomer;
-       // console.log(totalSold + " cones sold...")
+       console.log(conesRemaining + " cone(s) remaining.");
        if(conesRemaining < numEachCustomer){
            console.log("Cannot sell you " + numEachCustomer + " cones I only have " + conesRemaining + "...");
+           totalSold= totalSold + conesRemaining;
+           console.log("Thank you for buying the " + conesRemaining +  " cone(s).")
        }else{
            totalSold =  totalSold + numEachCustomer;
             console.log(totalSold + " cones sold...")
        }
 
    } while (totalSold < allCones);
+   console.log(totalSold + " total cones sold.");
+   console.log("Yay! I sold them all!");
 
 
     // 5 cones sold...  // if there are enough cones
