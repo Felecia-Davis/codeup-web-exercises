e8418884e69d548686404ac1ff6e1eddd7311413
Felecia Davis <felecia.m.davis1@gmail.com>
2021-02-15 23:12:04 -0500

    feat: completion of exercise planets-string.js along with html file split-join. Also some modification of break_and_continue.js

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -4,12 +4,12 @@
     var  userNumberEntered ;
     while (true) {
         userNumberEntered = Number(prompt("Enter an odd number between 1-50"));
-        if ((userNumberEntered % 2 !== 0) && ((userNumberEntered >1) && (userNumberEntered < 50))) {
-        break;
+        if ((userNumberEntered % 2 !== 0) && ((userNumberEntered > 1) && (userNumberEntered < 50))) {
+            break;
         }
         alert("This is not an odd or between 1 and 50");
         alert(" This is an odd number between 1 and 50");
-        for(var i = 1; i <= 50; i++) {
+        for (var i = 1; i <= 50; i++) {
             if (i % 2 === 0) {
                 continue;
             } else {
@@ -20,5 +20,5 @@
                 console.log(i);
             }
         }
-
+    }
 })();
