6b62235a8d1650ec48d5c4b70bf85203882a4951
Felecia Davis <felecia.m.davis1@gmail.com>
2021-04-06 15:00:34 -0400

    feat: add exercise on jquery selectors

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,3 +1,5 @@
 $(document).ready(function() {
-    alert( 'The DOM has finished loading!' );
+    // alert( 'The DOM has finished loading!' );
+    var contents = $('#you_all,.first_word, .list').css('color', 'blue');
+    alert(contents);
 });
